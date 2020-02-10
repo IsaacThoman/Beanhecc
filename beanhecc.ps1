7126d008982798d285a5d13be0d5b17e129006c4
isaacthoman <isaacthoman@outlook.com>
2020-02-10 09:27:11 -0800

    upload beanhecc

---
+++ beanhecc.ps1
@@ -0,0 +1,15 @@
+﻿$messagelimit = 58
+#set the messages to send
+sleep 3
+$textline = 0
+function hecc {
+$textline = $textline + 1
+$message = (gc hecc.txt | select-object -Index $textline) | Out-String
+[System.Windows.Forms.SendKeys]::SendWait($message)
+sleep 1
+if($textline -lt $messagelimit) {
+hecc
+}
+}
+hecc
+
