8a485155134883a35944b13b8f4c0f2a3949cf76
isaacthoman <isaacthoman@outlook.com>
2020-04-28 20:13:28 -0400

    Updated some stuff

--- beanhecc.ps1
+++ beanhecc.ps1
@@ -2,14 +2,45 @@
 #set the messages to send
 sleep 3
 $textline = 0
+$beanstop = 1
+
+
 function hecc {
 $textline = $textline + 1
 $message = (gc hecc.txt | select-object -Index $textline) | Out-String
 [System.Windows.Forms.SendKeys]::SendWait($message)
 sleep 1
 if($textline -lt $messagelimit) {
+if($beanstop = 0){
 hecc
 }
 }
+if($textline -gt $messagelimit) {
+$loopchecked = $CheckBox1.CheckState
+if($loopchecked -eq 'Checked'){
+$textline = 0
+
 hecc
+}
+$loopchecked = $CheckBox1.CheckState
+if($loopchecked -eq 'Unchecked'){
+beanstop
+}
+
+
+}
+
+}
+hecc
+
+function beanstop{
+$beanstop = 1
+
+
+}
+
+function openweb{
+start https://bean.bike/hecc
+}
+
 
---
+++ ui.ps1
@@ -0,0 +1,59 @@
+﻿<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
+.NAME
+    Untitled
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$BeanHecc                        = New-Object system.Windows.Forms.Form
+$BeanHecc.ClientSize             = '209,165'
+$BeanHecc.text                   = "BeanHecc"
+$BeanHecc.TopMost                = $false
+
+$Button1                         = New-Object system.Windows.Forms.Button
+$Button1.text                    = "Click here to edit your text"
+$Button1.width                   = 184
+$Button1.height                  = 30
+$Button1.location                = New-Object System.Drawing.Point(15,10)
+$Button1.Font                    = 'Microsoft Sans Serif,10'
+
+$TextBox1                        = New-Object system.Windows.Forms.TextBox
+$TextBox1.multiline              = $false
+$TextBox1.text                   = "1000"
+$TextBox1.width                  = 100
+$TextBox1.height                 = 30
+$TextBox1.location               = New-Object System.Drawing.Point(15,80)
+$TextBox1.Font                   = 'Microsoft Sans Serif,10'
+
+$Label1                          = New-Object system.Windows.Forms.Label
+$Label1.text                     = "Pause Between Lines (ms)"
+$Label1.AutoSize                 = $true
+$Label1.width                    = 25
+$Label1.height                   = 10
+$Label1.location                 = New-Object System.Drawing.Point(15,60)
+$Label1.Font                     = 'Microsoft Sans Serif,10'
+
+$CheckBox1                       = New-Object system.Windows.Forms.CheckBox
+$CheckBox1.text                  = "Loop"
+$CheckBox1.AutoSize              = $false
+$CheckBox1.width                 = 95
+$CheckBox1.height                = 20
+$CheckBox1.location              = New-Object System.Drawing.Point(15,110)
+$CheckBox1.Font                  = 'Microsoft Sans Serif,10'
+
+$Button2                         = New-Object system.Windows.Forms.Button
+$Button2.text                    = "View project on GitHub"
+$Button2.width                   = 184
+$Button2.height                  = 30
+$Button2.location                = New-Object System.Drawing.Point(15,130)
+$Button2.Font                    = 'Microsoft Sans Serif,10'
+
+$BeanHecc.controls.AddRange(@($Button1,$TextBox1,$Label1,$CheckBox1,$Button2))
+
+
+function openweb { }
+
+#Write your logic code here
+
+[void]$BeanHecc.ShowDialog()
